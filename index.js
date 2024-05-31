8b5f1c3925780722c6ba136d0008e4d63fcd1732
José M. Zavala González <jose.m.zavala.gonzalez@gmail.com>
2024-05-31 17:09:43 -0400

    Capturas con nombre

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // Basado en este tutorial:
 // https://www.bannerbear.com/blog/how-to-take-screenshots-with-puppeteer/
 
-const puppeteer = require('puppeteer');
+import puppeteer from 'puppeteer';
+
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc.js';
+import timezone from 'dayjs/plugin/timezone.js';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const tz = 'America/Puerto_Rico';
+dayjs.tz.setDefault(tz);
+
+const filename_datetime_format = 'YYYY-MM-DDTHH-mm-ssZZ';
 
 (async () => {
 
@@ -18,6 +30,12 @@ await page.setViewport({
     deviceScaleFactor: 2,
 });
 
+// Guardar fecha de ahora
+const now = dayjs();
+const filename_datetime = now.format(filename_datetime_format);
+console.log('now (ISO):', now.toISOString(), '(debe ser UTC)');
+console.log('filename_datetime:', filename_datetime, '(debe ser Puerto Rico)');
+
 // Abrir una URL
 const website_url = 'https://miluma.lumapr.com/outages/serviceStatus';
 await page.goto(website_url, { waitUntil: 'networkidle0' });
@@ -28,7 +46,9 @@ await page.waitForSelector('div.jss55', { visible: true });
 await new Promise(resolve => setTimeout(resolve, 500))
 
 // Tomar una captura de pantalla
-await page.screenshot({ path: 'screenshot.png' });
+const screenshot_filename = `luma_estatus_captura__${filename_datetime}.png`;
+console.log('Guardando como:', screenshot_filename);
+await page.screenshot({ path: 'capturas/'+screenshot_filename });
 
 // Cerrar el navegador
 await browser.close();
