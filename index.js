bc8093e9bf2c4289aa798d0536a3efd5d3e1cdfd
José M. Zavala González <jose.m.zavala.gonzalez@gmail.com>
2024-06-11 17:27:05 -0400

    retry attempts

--- index.js
+++ index.js
@@ -41,13 +41,41 @@ const filename_datetime = now.tz(tz).format(filename_datetime_format);
 console.log('now (ISO):', now.toISOString(), '(debe ser UTC)');
 console.log('filename_datetime:', filename_datetime, '(debe ser Puerto Rico)');
 
-// Abrir una URL
-const website_url = 'https://miluma.lumapr.com/outages/serviceStatus';
-await page.goto(website_url, { waitUntil: 'networkidle0' });
+let num_attempts = 0;
+const max_attempts = 3;
+let success = false;
 
-// Esperar por la tabla
-await page.waitForSelector('div.jss55', { visible: true });
 
+while (!success) {
+    num_attempts++;
+    if (num_attempts > max_attempts) {
+        console.log('Maximo de intentos alcanzado');
+        break;
+    }
+
+    console.log('Intento #', num_attempts);
+    // Abrir una URL
+    const website_url = 'https://miluma.lumapr.com/outages/serviceStatus';
+    await page.goto(website_url, { waitUntil: 'networkidle0' });
+
+    // Esperar por la tabla
+    await page.waitForSelector('div.jss55', { visible: true })
+    .then(() => {
+        console.log('Tabla encontrada');
+        success = true;
+    })
+    .catch((err) => {
+        console.log('Error:', err);
+    });
+}
+
+if (!success) {
+    console.log('No se pudo encontrar la tabla');
+    await browser.close();
+    throw new Error('No se pudo encontrar la tabla');
+}
+
+// Esperar un poco
 await new Promise(resolve => setTimeout(resolve, 500))
 
 // Tomar una captura de pantalla
